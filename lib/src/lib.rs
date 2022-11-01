14e6799d1588e9658f9e8d27e1a56ecf53114262
UMiKaMe0729 <shangtangcct+github@gmail.com>
2022-11-02 07:28:19 +0800

    wip

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,14 +1,33 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use std::path::PathBuf;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn module_load() {
+    let dirs = [".", "target/debug", "target/release"];
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    for dir in dirs
+        .iter()
+        .map(|f| PathBuf::from(f))
+        .filter(|f| f.exists() && f.is_dir())
+    {
+        for file in dir
+            .read_dir()
+            .unwrap()
+            .filter(|f| f.is_ok())
+            .map(|f| f.unwrap().path())
+            .filter(|f| f.is_file())
+            .filter(|f| {
+                f.extension().is_some()
+                    && ["dll", "so"].contains(&f.extension().unwrap().to_str().unwrap())
+            })
+            .filter(|f| f.file_stem().unwrap().ne("lib"))
+        {
+            println!("load file: {}", file.display());
+            println!("load by loading...");
+            load_libloading(&file);
+            println!("load by dlopen2...");
+            load_dlopen2(&file);
+        }
     }
 }
+
+fn load_libloading(path: &PathBuf) {}
+fn load_dlopen2(path: &PathBuf) {}

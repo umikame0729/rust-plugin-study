a181ae368759a50a4a7a9a30a3b1cc4b994dfca4
DennyChang <DennyChang@elytone.com.tw>
2022-11-02 13:16:13 +0800

    feat: with cargo make

--- build.rs
+++ build.rs
@@ -1,1 +1,6 @@
-fn main() {}
+use std::env;
+
+fn main() {
+    println!("cargo:warning=Target:{}", env::var("TARGET").unwrap());
+    println!("cargo:warning=Profile:{}", env::var("PROFILE").unwrap());
+}
